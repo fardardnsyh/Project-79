e9d72757f9e1414930e42d7346a3695fc2bca0bc
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-08-14 14:14:41 +0700

    Files upload

---
+++ client/src/pages/landingPage/Landing.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import main from "../../assets/images/main.svg";
+import { Wrapper } from "./landing.styles";
+import { Logo } from "../../components/index";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+
+      <div className="container page">
+        {" "}
+        <div className="info">
+          <h1>
+            Job <span>tracking</span> app
+          </h1>
+          <p>Track Your Job Search Process Now!</p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
